5f13d5d358ddb311e3c55972322e10f50bed36e3
Varshitha Urs H V <h.v.varshita.urs@hp.com>
2022-04-21 16:24:35 +0530

    AUTH Page

--- src/components/SignIn/AuthSign-In.tsx
+++ src/components/SignIn/AuthSign-In.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import { Button } from "@veneer/core";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { authenticate } from "../../redux/actions/authActions";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import authReducer from "../../redux/reducers/authReducer";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -16,10 +20,20 @@ const Wrapper = styled.div`
   }
 `;
 
-const AuthSignIn: React.FC = () => {
-  const navigate = useNavigate();
+interface AuthState {
+  authentication: typeof authReducer;
+}
 
-  const btnClick = () => {
+const AuthSignIn: React.FC = () => {
+  const auth: any = useSelector((state: AuthState) => state.authentication);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const btnClick = (e: any) => {
+    e.preventDefault();
+    dispatch(authenticate(auth.token));
+    const apiUrl = process.env.REACT_APP_PUBLIC_BACKEND_URL
+    window.location.href = `${apiUrl}/auth?next=${window.location.origin}/auth/in?token=`
     navigate("/plans")
   };
 
